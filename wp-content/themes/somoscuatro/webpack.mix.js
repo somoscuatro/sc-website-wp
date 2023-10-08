d2e63a9d1959805a25f66da6b95477593d11adcf
Luca Pipolo <luca@somoscuatro.es>
2023-10-08 09:32:15 +0200

    feat: scaffold somoscuatro custom theme

---
+++ wp-content/themes/somoscuatro/webpack.mix.js
@@ -0,0 +1,26 @@
+const fs = require( 'fs' );
+const mix = require( 'laravel-mix' );
+
+mix.setPublicPath( 'dist' );
+mix.setResourceRoot( '../' );
+
+const discover = ( dirs, type, excludedDirs = [] ) => {
+	let files = [];
+
+	dirs.forEach( ( dir ) => {
+		if ( excludedDirs.indexOf( dir ) === -1 ) {
+			fs.readdirSync( dir ).forEach( ( node ) => {
+				const nodePath = `${ dir }/${ node }`;
+				if ( fs.statSync( nodePath ).isFile() ) {
+					if ( nodePath.endsWith( type ) && ! node.startsWith( '.' ) && ! node.startsWith( '_' ) ) {
+						files.push( nodePath );
+					}
+				} else {
+					files = files.concat( discover( [ nodePath ], type ) );
+				}
+			} );
+		}
+	} );
+
+	return files;
+};
