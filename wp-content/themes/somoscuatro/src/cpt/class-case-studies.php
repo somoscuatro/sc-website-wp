958dd027914e8bbf60c1fbc1d8a4b20abaa8d5e8
Luca Pipolo <luca@somoscuatro.es>
2024-01-30 09:16:07 +0100

    refactor: case-studies excerpt fields

--- wp-content/themes/somoscuatro/src/cpt/class-case-studies.php
+++ wp-content/themes/somoscuatro/src/cpt/class-case-studies.php
@@ -23,7 +23,6 @@ class Case_Studies {
 	public static function init(): void {
 		self::register_cpt();
 		self::register_taxonomy();
-		self::register_acf_custom_fields();
 
 		// Removes post thumbnail from Case Study CPT.
 		add_filter( 'post_thumbnail_html', __CLASS__ . '::remove_post_thumbnail' );
@@ -42,7 +41,7 @@ class Case_Studies {
 				'menu_icon'    => 'data:image/svg+xml;base64,' . base64_encode(
 					'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"> <path fill-rule="evenodd" d="M2.25 2.25a.75.75 0 000 1.5H3v10.5a3 3 0 003 3h1.21l-1.172 3.513a.75.75 0 001.424.474l.329-.987h8.418l.33.987a.75.75 0 001.422-.474l-1.17-3.513H18a3 3 0 003-3V3.75h.75a.75.75 0 000-1.5H2.25zm6.04 16.5l.5-1.5h6.42l.5 1.5H8.29zm7.46-12a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6zm-3 2.25a.75.75 0 00-1.5 0v3.75a.75.75 0 001.5 0V9zm-3 2.25a.75.75 0 00-1.5 0v1.5a.75.75 0 001.5 0v-1.5z" clip-rule="evenodd" /> </svg>'
 				),
-				'supports'     => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+				'supports'     => array( 'title', 'editor', 'thumbnail', 'revisions', 'excerpt' ),
 				'show_in_rest' => true,
 			)
 		);
@@ -57,52 +56,6 @@ class Case_Studies {
 			'Case Studies Categories',
 			array( 'case-study' )
 		);
-	}
-
-	/**
-	 * Register Case Studies ACF Custom Fields.
-	 */
-	public static function register_acf_custom_fields(): void {
-		if ( function_exists( 'acf_add_local_field_group' ) ) {
-			acf_add_local_field_group(
-				array(
-					'key'      => 'group_cpt_case_studies',
-					'title'    => __( 'CPT: Case Studies', 'somoscuatro-theme' ),
-					'fields'   => array(
-						array(
-							'key'      => 'field_cpt_case_studies_image',
-							'label'    => __( 'Image', 'somoscuatro-theme' ),
-							'name'     => 'cpt_case_studies_image',
-							'type'     => 'image',
-							'required' => true,
-						),
-						array(
-							'key'      => 'field_cpt_case_studies_short_excerpt',
-							'label'    => __( 'Short Excerpt', 'somoscuatro-theme' ),
-							'name'     => 'cpt_case_studies_short_excerpt',
-							'type'     => 'textarea',
-							'required' => true,
-						),
-						array(
-							'key'      => 'field_cpt_case_studies_excerpt',
-							'label'    => __( 'Excerpt', 'somoscuatro-theme' ),
-							'name'     => 'cpt_case_studies_excerpt',
-							'type'     => 'textarea',
-							'required' => true,
-						),
-					),
-					'location' => array(
-						array(
-							array(
-								'param'    => 'post_type',
-								'operator' => '==',
-								'value'    => 'case-study',
-							),
-						),
-					),
-				)
-			);
-		}
 	}
 
 	/**
