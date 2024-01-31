2c800de46903f1ab99ad888e2afb941e39bd96f6
Luca Pipolo <luca@somoscuatro.es>
2024-01-31 14:55:22 +0100

    fix: hero block style

--- wp-content/themes/somoscuatro/src/blocks/hero/class-hero.php
+++ wp-content/themes/somoscuatro/src/blocks/hero/class-hero.php
@@ -34,17 +34,18 @@ class Hero extends Block {
 			'title'    => __( 'Block: Hero', 'somoscuatro-theme' ),
 			'fields'   => array(
 				array(
-					'key'      => 'field_' . self::$acf_block_prefix . '_headline',
-					'label'    => __( 'Headline', 'somoscuatro-theme' ),
-					'name'     => self::$acf_block_prefix . '_headline',
-					'type'     => 'text',
-					'required' => true,
+					'key'           => 'field_' . self::$acf_block_prefix . '_bg_color',
+					'label'         => __( 'Background Color', 'somoscuatro-theme' ),
+					'name'          => self::$acf_block_prefix . '_bg_color',
+					'type'          => 'color_picker',
+					'required'      => 1,
+					'return_format' => 'string',
 				),
 				array(
-					'key'      => 'field_' . self::$acf_block_prefix . '_text',
-					'label'    => __( 'Text', 'somoscuatro-theme' ),
-					'name'     => self::$acf_block_prefix . '_text',
-					'type'     => 'textarea',
+					'key'      => 'field_' . self::$acf_block_prefix . '_title',
+					'label'    => __( 'Title', 'somoscuatro-theme' ),
+					'name'     => self::$acf_block_prefix . '_title',
+					'type'     => 'text',
 					'required' => true,
 				),
 				array(
@@ -52,12 +53,6 @@ class Hero extends Block {
 					'label' => __( 'Image', 'somoscuatro-theme' ),
 					'name'  => self::$acf_block_prefix . '_image',
 					'type'  => 'image',
-				),
-				array(
-					'key'   => 'field_' . self::$acf_block_prefix . '_button',
-					'label' => __( 'Button', 'somoscuatro-theme' ),
-					'name'  => self::$acf_block_prefix . '_button',
-					'type'  => 'link',
 				),
 			),
 			'location' => array(
