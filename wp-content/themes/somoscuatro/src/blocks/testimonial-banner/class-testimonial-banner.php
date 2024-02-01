6ede77fad06b036f10ca3cec45934e109ebb4b43
Luca Pipolo <luca@somoscuatro.es>
2024-02-01 13:48:16 +0100

    feat: add testimonial-banner block

---
+++ wp-content/themes/somoscuatro/src/blocks/testimonial-banner/class-testimonial-banner.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Block's main functionality methods.
+ *
+ * @package somoscuatro-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Theme\Blocks\Testimonial_Banner;
+
+use Somoscuatro\Theme\Blocks\Block;
+
+/**
+ * Block main functionality.
+ */
+class Testimonial_Banner extends Block {
+
+	/**
+	 * The prefix used for ACF blocks.
+	 *
+	 * @var string
+	 */
+	protected static $acf_block_prefix = 'block_testimonial_banner';
+
+	/**
+	 * Gets the ACF Block fields.
+	 *
+	 * @return array The ACF Block fields.
+	 */
+	public static function get_acf_fields(): array {
+		return array(
+			'key'      => 'group_' . self::$acf_block_prefix,
+			'title'    => __( 'Block: Services', 'somoscuatro-theme' ),
+			'fields'   => array(
+				array(
+					'key'           => 'field_' . self::$acf_block_prefix . '_bg_color',
+					'label'         => __( 'Background Color', 'somoscuatro-theme' ),
+					'name'          => self::$acf_block_prefix . '_bg_color',
+					'type'          => 'color_picker',
+					'required'      => 1,
+					'return_format' => 'string',
+				),
+				array(
+					'key'         => 'field_' . self::$acf_block_prefix . '_testimonials',
+					'label'       => __( 'Testimonial', 'somoscuatro-theme' ),
+					'name'        => self::$acf_block_prefix . '_testimonials',
+					'type'        => 'relationship',
+					'required'    => true,
+					'post_type'   => array(
+						0 => 'testimonial',
+					),
+					'post_status' => array(
+						0 => 'publish',
+					),
+					'taxonomy'    => '',
+					'filters'     => array(
+						0 => 'search',
+					),
+					'min'         => 1,
+					'max'         => 1,
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param'    => 'block',
+						'operator' => '==',
+						'value'    => 'acf/testimonial-banner',
+					),
+				),
+			),
+		);
+	}
+}
