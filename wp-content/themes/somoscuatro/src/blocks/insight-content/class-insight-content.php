a23f2ff09af476889bf93fd1667a07fd1489a132
Luca Pipolo <luca@somoscuatro.es>
2024-02-08 15:23:59 +0100

    feat: add insight-content block

---
+++ wp-content/themes/somoscuatro/src/blocks/insight-content/class-insight-content.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Block's main functionality methods.
+ *
+ * @package somoscuatro-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Theme\Blocks\Insight_Content;
+
+use Somoscuatro\Theme\Blocks\Block;
+
+/**
+ * Block main functionality.
+ */
+class Insight_Content extends Block {
+
+	/**
+	 * The prefix used for ACF blocks.
+	 *
+	 * @var string
+	 */
+	protected static $acf_block_prefix = 'block_insight_content';
+
+	/**
+	 * Gets the ACF Block fields.
+	 *
+	 * @return array The ACF Block fields.
+	 */
+	public static function get_acf_fields(): array {
+		return array(
+			'key'      => 'group_' . self::$acf_block_prefix,
+			'title'    => __( 'Block: Case Study Content', 'somoscuatro-theme' ),
+			'fields'   => array(
+				array(
+					'key'      => 'field_' . self::$acf_block_prefix . '_content',
+					'label'    => __( 'Content', 'somoscuatro-theme' ),
+					'name'     => self::$acf_block_prefix . '_content',
+					'type'     => 'wysiwyg',
+					'required' => true,
+				),
+			),
+			'location' => array(
+				array(
+					array(
+						'param'    => 'block',
+						'operator' => '==',
+						'value'    => 'acf/insight-content',
+					),
+				),
+			),
+		);
+	}
+}
