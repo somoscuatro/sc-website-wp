e4e5159883a2b68d92f0111b264bb8939829c97c
Luca Pipolo <luca@somoscuatro.es>
2024-02-08 15:24:31 +0100

    feat: add single template

---
+++ wp-content/themes/somoscuatro/single.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The index template file.
+ *
+ * @package somoscuatro-theme
+ */
+
+declare(strict_types=1);
+
+namespace Somoscuatro\Theme;
+
+use Timber\Timber as TimberLibrary;
+
+$context = TimberLibrary::context();
+
+TimberLibrary::render( 'single.twig', $context );
