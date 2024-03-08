4b8d037f828345bce561772c84467a1a22df307a
Luca Pipolo <luca@somoscuatro.es>
2024-03-08 10:38:29 +0100

    fix: disable tiny-mce emoji cause fatal php error

--- wp-content/themes/somoscuatro/src/class-theme.php
+++ wp-content/themes/somoscuatro/src/class-theme.php
@@ -127,7 +127,6 @@ class Theme {
 		remove_action( 'admin_print_styles', 'print_emoji_styles' );
 		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-		add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
 	}
 
 	/**
