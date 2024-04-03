ee997688639e2dc459da4af60779eaa25d249e8e
Luca Pipolo <luca@somoscuatro.es>
2024-04-03 13:44:34 +0200

    refactor: adapt functions file to sc-starter-theme structure

--- wp-content/themes/somoscuatro/functions.php
+++ wp-content/themes/somoscuatro/functions.php
@@ -9,6 +9,10 @@ declare(strict_types=1);
 
 namespace Somoscuatro\Theme;
 
+use Somoscuatro\Theme\Attributes\Hook;
+use Somoscuatro\Theme\Dependency_Injection\Container as Dependencies;
+use Somoscuatro\Theme\Theme;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput
 	header( $_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found' );
@@ -18,14 +22,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Setup autoload.
 require_once __DIR__ . '/autoload.php';
 
-add_action( 'admin_init', __NAMESPACE__ . '\Admin::init' );
+// Register dependencies.
+$dependencies = new Dependencies();
+$dependencies->add( 'Theme', fn ( $dependencies ) => new Theme( $dependencies ) );
+$dependencies->add( 'Timber', fn () => new Timber() );
+$dependencies->add( 'ACF', fn() => new ACF() );
 
-add_action( 'after_setup_theme', __NAMESPACE__ . '\Timber::init', 9 );
-add_action( 'after_setup_theme', __NAMESPACE__ . '\Theme::after_setup_theme', 9 );
-add_action( 'init', __NAMESPACE__ . '\Theme::init' );
+// Register WordPress hooks.
+( new Hook( $dependencies ) )->register_hooks();
 
-add_action( 'plugins_loaded', __NAMESPACE__ . '\Theme::load_text_domain' );
-
-if ( defined( 'WP_CLI' ) && WP_CLI ) {
-	\WP_CLI::add_command( 'export-acf-blocks-fields', __NAMESPACE__ . '\Commands\Export_Acf_Blocks_Fields' );
-}
+// if ( defined( 'WP_CLI' ) && WP_CLI ) {
+// \WP_CLI::add_command( 'export-acf-blocks-fields', __NAMESPACE__ . '\Commands\Export_Acf_Blocks_Fields' );
+// }
