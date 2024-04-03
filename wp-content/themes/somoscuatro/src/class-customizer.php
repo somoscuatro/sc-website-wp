f20f9fca576c6d108335b71bfd67cad88cadb9e9
Luca Pipolo <luca@somoscuatro.es>
2024-04-03 14:50:48 +0200

    feat: add google tag manager controls to the customizer

--- wp-content/themes/somoscuatro/src/class-customizer.php
+++ wp-content/themes/somoscuatro/src/class-customizer.php
@@ -178,6 +178,44 @@ class Customizer {
 	}
 
 	/**
+	 * Adds Google Tag Manager controls to the customizer.
+	 *
+	 * @param \WP_Customize_Manager $wp_customize WP_Customize_Manager instance.
+	 */
+	#[Action( 'customize_register' )]
+	public function add_customizer_gtm_controls( \WP_Customize_Manager $wp_customize ) {
+		// Section.
+		$wp_customize->add_section(
+			'gtm',
+			array(
+				'title'      => __( 'Google Tag Manager', 'somoscuatro-theme' ),
+				'priority'   => 35,
+				'capability' => 'edit_theme_options',
+			)
+		);
+		$wp_customize->add_setting(
+			'gtm_id',
+			array(
+				'default'    => '',
+				'type'       => 'theme_mod',
+				'capability' => 'edit_theme_options',
+			)
+		);
+		$wp_customize->add_control(
+			new \WP_Customize_Control(
+				$wp_customize,
+				'gtm_id',
+				array(
+					'label'    => __( 'GTM ID', 'somoscuatro-theme' ),
+					'section'  => 'gtm',
+					'settings' => 'gtm_id',
+					'type'     => 'text',
+				),
+			),
+		);
+	}
+
+	/**
 	 * Retrieves all pages in site.
 	 *
 	 * @return array List of pages and their IDs.
