bc7253132cccca5293c2059b882d77bfab39b97f
Luca Pipolo <luca@somoscuatro.es>
2024-07-22 13:37:20 +0200

    feat: add plugin acfml

---
+++ wp-content/plugins/acfml/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '2.1.3',
+        'version' => '2.1.3.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bc7a9e530ba8e973f31648800c32f6e14a40c9fc',
+        'name' => 'acfml/advanced-custom-fields-multilingual',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'acfml/advanced-custom-fields-multilingual' => array(
+            'pretty_version' => '2.1.3',
+            'version' => '2.1.3.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bc7a9e530ba8e973f31648800c32f6e14a40c9fc',
+            'dev_requirement' => false,
+        ),
+        'wpml-shared/wpml-lib-dependencies' => array(
+            'pretty_version' => '0.1.6',
+            'version' => '0.1.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpml-shared/wpml-lib-dependencies',
+            'aliases' => array(),
+            'reference' => '723824a61bfec7dd535afcd7260fad50d8bfe6e8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
