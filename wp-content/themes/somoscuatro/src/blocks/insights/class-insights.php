ef7dd50890e9a948816a428346bdd27f5b92dd23
Joan López <joan@somoscuatro.es>
2024-07-29 16:32:38 +0200

    feat: add heading type select to block insights

--- wp-content/themes/somoscuatro/src/blocks/insights/class-insights.php
+++ wp-content/themes/somoscuatro/src/blocks/insights/class-insights.php
@@ -49,6 +49,19 @@ class Insights extends Block {
 					'required' => true,
 				),
 				array(
+					'key'      => 'field_' . self::$acf_block_prefix . '_heading_type',
+					'label'    => __( 'Heading type', 'somoscuatro-theme' ),
+					'name'     => self::$acf_block_prefix . '_heading_type',
+					'type'     => 'select',
+					'choices'  => array(
+						'h1' => 'h1',
+						'h2' => 'h2',
+						'h3' => 'h3',
+					),
+					'default'  => 'h2',
+					'required' => true,
+				),
+				array(
 					'key'           => 'field_' . self::$acf_block_prefix . '_number_of_posts',
 					'label'         => __( 'Number of Posts', 'somoscuatro-theme' ),
 					'type'          => 'range',
