bf7e65f6b98d7ba32523e38e8ae25502919020dd
Luca Pipolo <luca@somoscuatro.es>
2024-10-25 11:30:04 +0200

    chore: remove plugin automattic-for-agencies-client

--- wp-content/plugins/automattic-for-agencies-client/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '732937aa674bc15ec62a');
--- wp-content/plugins/automattic-for-agencies-client/jetpack_vendor/automattic/jetpack-connection/dist/identity-crisis.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '71f14ae42588b9c8b35b');
